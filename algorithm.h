67874ca14fcd14c9b191733e13038bd9d1499084
Bierbou <bierbou@gmx.net>
2013-07-15 18:25:16 +0200

    Files deleted because they are unimportant

--- algorithm.h
+++
@@ -1,9 +0,0 @@
-#ifndef ALGORITHM_H
-#define ALGORITHM_H
-
-#include<vector>
-
-
-std::vector<float> algorithm(float n ,std::vector<float> v);
-
-#endif
--- bin.h
+++
@@ -1,8 +0,0 @@
-#ifndef BIN_H
-#define BIN_H
-
-#include<vector>
-
-std::vector<int> binning(std:: vector<float> v);
-
-#endif
